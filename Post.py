de73b859c49fba1cc745f89d0afaae141c52ed4b
Adi <kindilhome@gmail.com>
2025-01-23 16:55:58 +0200

    Class Post created

--- Post.py
+++ Post.py
@@ -1,1 +1,13 @@
-print("HI")
+class Post:
+    def __init__(self, username, location, description, likes_counter, comments):
+        self.username = username
+        self.location = location
+        self.description = description
+        self.likes_counter = likes_counter
+        self.comments = comments
+    def add_like(self):
+        self.likes_counter += 1
+    def add_comment_text(self, text):
+        if isinstance(comment, text):
+            self.comments.append(text)
+
